2ba28622f9ecfcbead90f6ed04bc0c30523d4049
Krishna5720 <leelakrishna.y@smartgig.tech>
2024-10-15 00:42:11 +0530

    add mismatches

--- job-siege/src/App.js
+++ job-siege/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./LoginRegister/Login";
 import Home from "./HomePage/Home";
+import Register from "./LoginRegister/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" Component={Login}></Route>
           <Route path="homePage" Component={Home}></Route>
+          <Route path="/register" Component={Register}></Route>
         </Routes>
       </Router>
     </>
