2223fe0e7c4441ba6255a5a0c490844ca43507ab
Krishna5720 <leelakrishna.y@smartgig.tech>
2024-10-14 23:29:50 +0530

    Implement Header

--- job-siege/src/HomePage/Home.jsx
+++ job-siege/src/HomePage/Home.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React  from 'react'
+import './Home.css';
+import applogo from "../Images/applogo.png"
+import line from "../Images/line.png"
+import profile from "../Images/profile.png"
 
 function Home() {
+
+
   return (
-    <div>
-      Hello world
-    </div>
+    <nav class="navbar">
+      <div class="nav-left">
+          <img src={profile} alt="" class="profile-pic"/>
+          <span class="username">Username</span>
+          <img src={line} alt="" className='lines'/>
+      </div>
+      
+      <div class="nav-middle">
+          <img src={applogo} alt="" class="app-logo"/>
+          <span class="app-name">
+              <span style={{ color: "#975842" }}>J</span>ob{" "}
+              <span style={{ color: "#975842" }}>S</span>iege
+          </span>
+      </div>
+      
+    
+      <div class="nav-right">
+          <div className='nav-right desktop'>
+            <img src={line} alt="" className='lines'/>
+            <a href="/homepage" class="nav-link">Home</a>
+            <img src={line} alt="" className='lines'/>
+            <a href="/homepage" class="nav-link">About Us</a>
+            <img src={line} alt="" className='lines'/>
+            <a href="/homepage" class="nav-link">Contact Us</a>
+          </div>
+      </div>
+    </nav>
   )
 }
 
