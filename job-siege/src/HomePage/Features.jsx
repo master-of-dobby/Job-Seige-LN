fc1a389ee89094cc0606a80cb4ba046bde9a74a6
Krishna5720 <leelakrishna.y@smartgig.tech>
2024-10-16 18:00:18 +0530

    Set up the template the features section

---
+++ job-siege/src/HomePage/Features.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Features.css"
+
+function Features() {
+  return (
+    <div class="container">
+        <div class="box">
+            <div class="info-block">
+                <div class="info-header">
+                    <div>
+                        <h2>Heading</h2>
+                    </div>
+                    <div>
+                        <h2>Heading</h2>
+                    </div>
+                </div>
+                <div class="info-body">
+                    <p>This is the information about the heading. You can include any text or details here to provide context.</p>
+                </div>
+            </div>
+        </div>
+        <div class="box">Block 2</div>
+        <div class="box">Block 3</div>
+    </div>
+  )
+}
+
+export default Features
--- job-siege/src/HomePage/Home.jsx
+++ job-siege/src/HomePage/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./Home.css"
 import Header from './Header'
 import HeroSec from './HeroSec'
+import Features from './Features'
 
 function Home() {
   return (
     <div className='entire'>
         <Header/>
         <HeroSec/>
+        <Features/>
     </div>
   )
 }
