c74bf65ffd5a4800e4b2b68a65cc5f500c7f5bd0
master-of-dobby <naveenoffcial777@gmail.com>
2024-10-25 22:36:36 +0530

    added track jobs page

--- job-siege/src/HomePage/HeroSec.jsx
+++ job-siege/src/HomePage/HeroSec.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import image from '../Images/heroSection.jpeg'
-import "./HeroSec.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import image from "../Images/heroSection.jpeg";
+import "./HeroSec.css";
 
 function HeroSec() {
+  const navigate = useNavigate();
+
+  const handleTrackIt = () => {
+    navigate("/track-jobs");
+  };
   return (
     <div className="hero-container">
       <div className="hero-image">
-        <img
-          src={image}
-          alt="Hero"
-        />
+        <img src={image} alt="Hero" />
       </div>
       <div className="hero-text">
         <p>
-          Effectively tracking and managing job applications is equally critical as the process of applying itself. 
+          Effectively tracking and managing job applications is equally critical
+          as the process of applying itself.
         </p>
+        <button onClick={handleTrackIt} className="track-it">
+          Track it
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default HeroSec
+export default HeroSec;
